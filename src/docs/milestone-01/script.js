18ecd93f8593e476946a85474ce2eeaab2824f58
Van Dong Vo <dongvan18032003@gmail.com>
2024-04-02 13:13:52 -0400

    add authors, interactive bio, modified styles.css, add new navigation

---
+++ src/docs/milestone-01/script.js
@@ -0,0 +1,19 @@
+let teamMemberDiv = document.querySelectorAll(".team-member");
+let teamMemberName = document.querySelectorAll(".team-member h3");
+let teamMemberBio = document.querySelectorAll(".team-member p");
+
+for (let i = 0; i < teamMemberDiv.length; ++i) {
+    teamMemberBio[i].style.display = 'none';
+}
+
+for (let i = 0; i < teamMemberDiv.length; ++i) {
+    teamMemberDiv[i].addEventListener('click', () => {
+        if (teamMemberBio[i].style.display === 'none') {
+            teamMemberBio[i].style.display = 'block';
+            teamMemberDiv[i].style["align-self"] = 'stretch';
+        } else {
+            teamMemberBio[i].style.display = 'none';
+            teamMemberDiv[i].style["align-self"] = 'flex-start';
+        }
+    })
+}
