77f53d38d353ceb9477c64ad9830b0aae09af76a
Van Dong Vo <dongvan18032003@gmail.com>
2024-04-02 18:29:19 -0400

    add profile pictures

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,9 +1,10 @@
 let teamMemberDiv = document.querySelectorAll(".team-member");
 let teamMemberName = document.querySelectorAll(".team-member h3");
-let teamMemberBio = document.querySelectorAll(".team-member p");
+let teamMemberBio = document.querySelectorAll(".team-member .bio");
 
 for (let i = 0; i < teamMemberDiv.length; ++i) {
     teamMemberBio[i].style.display = 'none';
+    teamMemberBio[i].style.border = '0px';
 }
 
 for (let i = 0; i < teamMemberDiv.length; ++i) {
