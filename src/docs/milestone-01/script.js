834ae7d80d1dbf53ed715196c12f680c5bc46e5a
Van Dong Vo <dongvan18032003@gmail.com>
2024-04-02 19:08:59 -0400

    change index.html to pass validation-html

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -2,7 +2,7 @@ let teamMemberDiv = document.querySelectorAll(".team-member");
 let teamMemberName = document.querySelectorAll(".team-member h3");
 let teamMemberBio = document.querySelectorAll(".team-member .bio");
 
-let teamHTML = [`<img src="assets/rubens.jpeg">
+let teamHTML = [`<img src="assets/rubens.jpeg" alt="profile pic">
 <p>I am a motivated student and studying Computer Science,
 and am currently a junior. I have had an interest in Computer
 Science since discovering it in high school, and have a passion for learning
@@ -16,13 +16,13 @@ Science since discovering it in high school, and have a passion for learning
         databases, using MongoDB. For the front-end, 
          I am only a little familiar with React, 
          but will strive to learn more. </p>`,
-`<img src="assets/znche.jpg">
+`<img src="assets/znche.jpg" alt="profile pic">
 <p>Hello, my name is Zih and I am a junior majoring in Informatics. I have experiences with programming languages such as Python, Java, Javascript, and R. My favorite would have to be R! In a previous course titled Info 101 (The introduction to informatics), I worked on a web development project where my group incorporated HTML. I’m interested in web development/application and practicing more of it this semester as well as the rest of my time at UMass.
 </p>`,
-`<img src="assets/van.jpeg">
+`<img src="assets/van.jpeg" alt="profile pic">
 <p>I am a Junior in Computer Science. I have experience in Java, Javascript, C++ and Python. I know a little bit about web development, but I have never worked on frontend. I have some experience with backend, MySQL, Spring Boot from CS 320 course. Through this course, I hope that I will become familiar with frontend stuff and sharpen my backend skills.
 </p>`,
-`<img src="assets/quan.JPG">
+`<img src="assets/quan.JPG" alt="profile pic">
 <p>I am a junior in Computer Science. Aside from what we learn in this course(HTML/CSS, Node.JS), I know Java, Javascript, Python, C/C++ and have used SpringBoot framework before. I also have used PostGreSQL, but nothing too advanced. I have some experience with building a web application on the backend side from my previous course. Currently, I am also learning more about Node.JS, React, and MongoDB by myself, but I am not exactly really good at them yet. I am hoping that I will gain more experience in both front-end and back-end stuff from this course.
 </p>`]
 
