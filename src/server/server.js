c23dcae1d324110729fcdc3a67a9b88aad115e56
QDVO <quanvan18032003@gmail.com>
2024-05-14 19:40:41 -0400

    put everything together

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,90 @@
 import express from "express";
 import logger from "morgan";
 import * as db from "./db.js";
+import * as serverdb from "./serverdb.js";
 // import { saveData, getData, removeData } from "./db.js";
 
 
 const headerFields = { "Content-Type": "text/html" };
+//Name = course name numCorrect = num questions correct. totalQuestions = initial total number of questions for this course
+async function createCourse(response, name, numCorrect, totalQuestions){
+  if (name === undefined || totalQuestions === 0) {
+      response.writeHead(400, headerFields);
+      response.write("<h1>Invalid Entry</h1>");
+      response.end();
+  } 
+  else
+   {
+      try
+      {
+          await serverdb.saveCourse(name, numCorrect, totalQuestions); 
+          response.writeHead(200, headerFields);
+          response.write(`<h1>Course ${name} Created</h1>`);
+          response.end();
+      } 
+      catch (err)
+      {
+          console.log(err);
+          response.writeHead(500, headerFields);
+          response.write("<h1>Internal Server Error</h1>");
+          response.write("<p>Unable to create course</p>");
+          response.write(`<p>This is likely a duplicate course name!</p>`);
+          response.end();
+      }
+  }
+}
+
+//reads stats of a course
+
+async function readCourse(response, name) {
+  try {
+    const course = await serverdb.loadCourse(name);
+    response.write(JSON.stringify(course));
+    response.end()
+  } catch (err) {
+    response.writeHead(404, headerFields);
+    // response.write(`<h1>Course ${name} Not Found</h1>`);
+    response.write(JSON.stringify({error: "error occured"})); 
+    response.end();
+   
+  }
+}
+
+
+//updates stats on a course
+
+async function updateCourse(response, name, numCorrect, totalQuestions) {
+  try {
+    const course = await serverdb.loadCourse(name);
+    course.numCorrect = numCorrect
+    course.totalQuestions = totalQuestions
+    await serverdb.modifyCourse(course);
+    response.writeHead(200, headerFields);
+    response.write(`<h1>Course ${course._id} Updated</h1>`);
+    response.end();
+  } catch (err) {
+    response.writeHead(404, headerFields);
+    response.write(`<h1>Course ${name} Not Found Or invalid Entry</h1>`);
+    response.end();
+  }
+}
+
+//deletes course stats
+
+async function deleteCourse(response, name) {
+  try {
+    const course = await serverdb.loadCourse(name);
+    response.writeHead(200, headerFields);
+    response.write(`<h1>Course ${course._id} Deleted</h1>`);
+    response.end();
+    serverdb.removeCourse(course);
+  } catch (err) {
+    response.writeHead(404, headerFields);
+    response.write(`<h1>Course ${name} Not Found</h1>`);
+    response.end();
+  }
+}
+
 
 
 async function get(response, user_id) {
@@ -85,8 +165,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-// The following code handles static file requests for the client-side code.
-// You do not need to modify this code. It serves the client-side files from
+// The following code handles static file requests for the client-side code. It serves the client-side files from
 // the `src/client` directory.
 app.use(express.static("src/client"));
 
@@ -97,6 +176,40 @@ const MethodNotAllowedHandler = async (request, response) => {
 
 
 // routes
+app
+.route("/read")
+.get(async (request, response) => {
+  const options = request.query;
+  console.log('options...')
+  console.log(options)
+  readCourse(response, options.name);
+})
+.all(MethodNotAllowedHandler);
+
+app
+.route("/create")
+.post(async (request, response) => {
+  const options = request.query;
+  createCourse(response, options.name, options.numCorrect, options.totalQuestions);
+})
+.all(MethodNotAllowedHandler);
+
+app
+.route("/update")
+.put(async (request, response) => {
+  const options = request.query;
+  updateCourse(response, options.name, options.numCorrect, options.totalQuestions);
+})
+.all(MethodNotAllowedHandler);
+
+app
+.route("/delete")
+.delete(async (request, response) => {
+  const options = request.query;
+  deleteCourse(response, options.name);
+})
+.all(MethodNotAllowedHandler);
+
 app
  .route("/get")
  .get(async (request, response) => {
---
+++ src/server/serverdb.js
@@ -0,0 +1,21 @@
+import PouchDB from "pouchdb";
+
+const db = new PouchDB('serverdb') // this db will hold statistics for user courses
+
+export async function removeCourse(name) {
+    db.remove(name);
+  }
+  
+  export async function loadCourse(name) {
+    const course = await db.get(name);
+    return course;
+  }
+  
+
+  export async function modifyCourse(doc) {
+    await db.put(doc);
+  }
+
+  export async function saveCourse(name, numCorrect, totalQuestions) {
+    await db.put({ _id: name, numCorrect, totalQuestions });
+  }
